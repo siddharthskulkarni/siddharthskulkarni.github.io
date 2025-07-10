2530030f9d3157c92808700bc6254f166281887a
Siddharth Kulkarni <10siddharthk@gmail.com>
2025-07-10 15:59:54 -0700

    Handle refresh

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,12 @@ import App from './App.jsx'
 // Make Buffer available globally
 window.Buffer = Buffer;
 
+const path = sessionStorage.getItem('redirectPath');
+if (path) {
+  sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', path);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
